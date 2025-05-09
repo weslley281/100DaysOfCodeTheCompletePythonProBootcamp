1a3755c98fe0abeb8fd23ec57015197d6b9c4daa
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2025-05-09 19:39:09 -0400

    n

--- Day2/task.py
+++ Day2/task.py
@@ -1,10 +1,14 @@
 name = "Weslley"
 lastName = "Ferraz"
+age = 29
+weight = 73.4
+isCheatedOn = False
+lengthName = int(len(name))
 
 print(len(name))
 print(name[0], name[1], name[2])
 print(name + " " + lastName)
 
-print(12 + 12)
-
-len(name)
+print(12 + lengthName)
+print(len(name))
+print(type(name), type(age), type(weight), type(isCheatedOn))
